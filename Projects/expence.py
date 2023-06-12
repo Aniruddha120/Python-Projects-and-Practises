619a341844722b41484d2388614ff8477730145d
Aniruddha Sarkar <34673269+Aniruddha120@users.noreply.github.com>
2023-06-13 01:20:31 +0600

    Update expence.py

--- Projects/expence.py
+++ Projects/expence.py
@@ -1,9 +1,9 @@
-tom_items = [100,200,300]
-joe_items = [400,500,600]
+tom_items = [100,200,300,400]
+joe_items = [400,500,600,500]
 total = 0
 
 for item in tom_items:
 
     total = total+tom_items
 print('Total expence of Tom ', total)
-print("OK")
+print("OK",total+total )
