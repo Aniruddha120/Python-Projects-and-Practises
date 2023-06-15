ac38ec880dee5c84b91bc612fd04c5a8a3ecb7f1
Aniruddha Sarkar <34673269+Aniruddha120@users.noreply.github.com>
2023-06-15 23:58:41 +0600

    Update lesson 2.py

--- Object Oriented Programming/lesson 2.py
+++ Object Oriented Programming/lesson 2.py
@@ -13,4 +13,12 @@ item1 = Vehicle(10, 1000, 'car')
 item2 = Vehicle (20,500,'normal')
 item1.calculate()
 item2.calculate()
+def __init__(self, quantity :float , value : float, name : str ):
+        assert quantity>= 0 ,f'The quantity {quantity} is not greater then 0'
+        self.q = quantity
+        self.a = value
+        self.n = name
+        self.m = age
+item1 = Vehicle(10, 1000, 'car')
+item2 = Vehicle (20,500,'normal')        
         
