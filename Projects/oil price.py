74fd8d889d720c1214ca7a0aa5d0f9ec7644cc17
Aniruddha Sarkar <34673269+Aniruddha120@users.noreply.github.com>
2023-06-16 23:36:09 +0600

    Add files via upload

---
+++ Projects/oil price.py
@@ -0,0 +1,10 @@
+milage = [100,200,300,400]
+oil price = [400,500,600,500]
+
+total = 0
+
+for item in fees_items:
+
+    total = total+tom_items
+print('Total expence of oil ', total)
+print("OK",total+total )
