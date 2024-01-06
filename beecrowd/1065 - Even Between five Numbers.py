b0658e2ab39ec754c430e7be29305b6bf7ce77dc
Aniruddha Sarkar <34673269+Aniruddha120@users.noreply.github.com>
2024-01-06 11:39:49 +0600

    Create 1065 - Even Between five Numbers.py

---
+++ beecrowd/1065 - Even Between five Numbers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = 0
+
+if a%2==0:
+    f=f+1
+if b%2==0:
+    f=f+1
+if c%2==0:
+    f=f+1
+if d%2==0:
+    f=f+1
+if e%2==0:
+    f=f+1
+print(f,'valores pares')
