63c408c29eac01a55c2517ac4348d036157e47aa
Aniruddha Sarkar <34673269+Aniruddha120@users.noreply.github.com>
2024-01-10 05:16:59 +0000

    daily beecrowd

---
+++ beecrowd/1038 snack.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+X,Y=list(map(float,input().split()))
+if X == 1:
+    c= Y*4.00
+    print('Total: R$ ''%.2f'%c)
+if X == 2:
+    c= Y*4.5
+    print('Total: R$ ''%.2f'%c)
+if X == 3:
+    c= Y*5
+    print('Total: R$ ''%.2f'%c)
+if X == 4:
+    c= Y*2
+    print('Total: R$ ''%.2f'%c)
+if X == 5:
+    c= Y*1.5
+    print('Total: R$ ''%.2f'%c)
